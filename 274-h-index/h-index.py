ba9e6ed3675b44f175e4ce1d0721557484b1e2ce
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-14 22:20:48 -0400

    Time: 3 ms (26.50%) | Memory: 19.5 MB (9.85%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,9 +1,13 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        aux = [c for c in sorted(citations) if c!=0]
-        h=len(aux)
-        for i, amount in enumerate(aux):
-            if amount >= len(aux[i:]):
-                h=min(len(aux[i:]), amount)
-                return h
-        return h
+        n = len(citations)
+        aux = [0]*(n+1)
+        for c in citations:
+            if c>=n: c=n
+            aux[c]+=1
+        k=n
+        s=aux[n]
+        while k> s:
+            k-=1
+            s+=aux[k]
+        return k
