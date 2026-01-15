996f54052df41fc3026b4a756ca8125428fffd75
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-15 16:35:10 -0400

    Time: 0 ms (100.00%) | Memory: 2.7 MB (27.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.rs
+++ 125-valid-palindrome/valid-palindrome.rs
@@ -1,61 +1,26 @@
 impl Solution {
-    pub fn is_palindrome_old(s: String) -> bool {
-        let n: usize= s.len() ;
+    pub fn is_palindrome(s: String) -> bool {
+        let chars: Vec<char> = s.chars().collect();
         let mut start: usize = 0;
-        let mut end: usize= n-1;
+        let mut end: usize= chars.len()-1;
 
-        let half: usize = n>>1;
-        let mut first: char;
-        let mut last: char;
-        let mut valid = true;
-        for i in 0..half {
-            first = s.chars().nth(start).unwrap();
-            last = s.chars().nth(end).unwrap();
-            while (!first.is_alphanumeric() || first == ' ') && start <n-1 {
+        while start<end {
+            while (!chars[start].is_alphanumeric()) && start < end{
                 start+=1;
-                first = s.chars().nth(start).unwrap();
 
             }
-            while (!last.is_alphanumeric() || last == ' ') && end > 0{
+            while (!chars[end].is_alphanumeric()) && start<end {
                 end-=1;
-                last= s.chars().nth(end).unwrap();
             }
-                println!("{:?}, {:?}", first, last);
 
-            if first.eq_ignore_ascii_case(&last) {
-            }
-            else {
+            if !chars[start].eq_ignore_ascii_case(&chars[end]) {
                 return false;
-
             }
-            start+=1;
-            end-=1;
-
+            
+            start += 1;
+            if end > 0 { end -= 1; } 
         }           
         
-        return valid;
-    }
-    pub fn is_palindrome(s: String) -> bool {
-        let mut formatted = s.to_lowercase();
-        formatted = formatted.chars().filter(|c| c.is_alphanumeric() && *c != ' ').collect();
-        let n = formatted.len();
-        let mut end = n-1;
-        let mut start = 0;
-        let mut evaluated_all= false;
-        let mut first: char;
-        let mut last: char;
-        let mut valid= true;
-        let half = n>>1;
-        for i in 0..half {
-            first = formatted.chars().nth(start).unwrap();
-            last = formatted.chars().nth(end).unwrap();
-            start+=1;
-            end-=1;
-            println!("{:?}, {:?}", first, last);
-            if !first.eq_ignore_ascii_case(&last){
-                return false;
-            }
-        }
-        return valid;
+        return true;
     }
 }
