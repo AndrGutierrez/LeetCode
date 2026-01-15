867cf8309df5b8f5732eea1760d4b7d3a926c411
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-14 22:08:57 -0400

    Time: 24 ms (1.35%) | Memory: 2.3 MB (41.89%) - LeetSync

--- 274-h-index/h-index.rs
+++ 274-h-index/h-index.rs
@@ -2,7 +2,10 @@ use std::collections::HashMap;
 impl Solution {
     pub fn h_index(citations: Vec<i32>) -> i32 {
         let mut c: HashMap<i32, i32>= HashMap::new();
-        for amount in citations {
+        for &amount in citations.iter() {
+            let mut m = amount;
+            let n = citations.len() as i32;
+            if amount >=n{m=n}
             for i in 0..amount+1{
                 let mut number = c.entry(i).or_insert(0);
                 *number+=1;
