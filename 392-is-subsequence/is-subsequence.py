0356934a0c9795ef9e1f0159804d5951c8985b1f
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-15 23:14:25 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (12.01%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,15 @@
+from collections import deque
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        arr =list(s)
+        full_text = list(t)
+
+        q = deque(arr)
+        print(q)
+        for char in full_text:
+            if not q: return True
+            if q[0]  == char:
+                q.popleft()
+
+        if q: return False
+        else: return True
