82640eda4a55380c6659d84a2975002147e3e3eb
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-18 23:42:24 -0400

    Time: 27 ms (47.04%) | Memory: 25.2 MB (45.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -9,15 +9,16 @@ class Solution:
                 zeros.append(i) 
                 continue
             mult*=num
-        
+        if len(zeros)>=2:
+            return [0]*len(nums)
+        elif len(zeros)>=1:
+            return [0]*(zeros[0])+ [mult] + [0]*(len(nums)-zeros[0]-1)
+ 
         for i, num in enumerate(nums, 1):
             if num == 0: 
                 res.append(mult)
                 continue                
             n = int(mult*(num**-1))
             res.append(n)
-        if len(zeros)>=2:
-            return [0]*len(nums)
-        elif len(zeros)>=1:
-            return [0]*(zeros[0])+ [mult] + [0]*(len(nums)-zeros[0]-1)
+            
         return res
