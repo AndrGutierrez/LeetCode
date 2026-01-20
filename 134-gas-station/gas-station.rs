6f8aa8dac801c1701cf9e7669c53dd574b15b978
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-20 00:46:18 -0400

    Time: 0 ms (100.00%) | Memory: 3.8 MB (5.75%) - LeetSync

--- 134-gas-station/gas-station.rs
+++ 134-gas-station/gas-station.rs
@@ -3,8 +3,8 @@ impl Solution {
         let n: i32 = gas.len() as i32;
         let mut differences: Vec<i32> = Vec::<i32>::new();
         let mut total = 0;
-        let mut cycle_completed = false;
         let mut nodes_visited=0;
+        
         for i in 0..n {
             let idx: usize=i as usize;
             let difference = gas[idx]-cost[idx];
@@ -15,10 +15,10 @@ impl Solution {
         if total >=0 {
             let mut i = 0;
             let mut carry = 0;
-            while !cycle_completed{
+            while true{
                 if i >=n{i=0};
-                carry+=differences[i as usize] ;
-                if carry <0{
+                carry+=differences[i as usize];
+                if carry < 0 {
                     nodes_visited=0;
                     carry = 0;
                 }
