672321ac98d5d411482910aec44e395ee0becffb
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-20 00:39:20 -0400

    Time: 1 ms (21.84%) | Memory: 3.9 MB (5.75%) - LeetSync

---
+++ 134-gas-station/gas-station.rs
@@ -0,0 +1,47 @@
+impl Solution {
+    pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
+        let n: i32 = gas.len() as i32;
+        let mut differences: Vec<i32> = Vec::<i32>::new();
+        let mut max_difference=0;
+        let mut max_index:i32=0;
+        let mut differences: Vec<i32> = Vec::<i32>::new();
+        let mut total = 0;
+        let mut cycle_completed = false;
+        let mut nodes_visited=0;
+        for i in 0..n {
+            let idx: usize=i as usize;
+            let difference = gas[idx]-cost[idx];
+            if difference>max_difference{
+                max_difference = difference;
+                max_index=i;
+            }
+            differences.push(difference);
+            total+=difference;
+        }
+    
+        if total >=0 {
+            let mut i = 0;
+            let mut carry = 0;
+            while !cycle_completed{
+                if i >=n{i=0};
+                carry+=differences[i as usize] ;
+                if carry <0{
+                    nodes_visited=0;
+                    carry = 0;
+                }
+                else{
+                    nodes_visited+=1
+                }
+                if nodes_visited >=n {
+                    break;
+                }
+                i+=1;
+            }
+            if i+1>=n {return 0}
+            return i+1;
+        }
+        else{
+            return -1
+        }
+    }
+}
