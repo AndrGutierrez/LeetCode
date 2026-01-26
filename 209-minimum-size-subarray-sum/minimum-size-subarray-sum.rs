823122253bfd6cbe048fe6192edbcd0d2c4162e9
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-25 23:12:33 -0400

    Time: 460 ms (5.52%) | Memory: 2.9 MB (96.32%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.rs
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.rs
@@ -8,24 +8,26 @@ use std::cmp::min;
 impl Solution {
     pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
         let mut window_size: i32 = 4;
-        let mut min_window_size: i32 = 0;
+        let mut min_window_size: i32 = i32::MAX;
         let mut left: i32 = 0;
         let n: i32 = nums.len() as i32;
         for right in 0..n {
             let mut sum: i32 = nums[left as usize..(right+1) as usize].iter().sum();
             while sum >= target {
                 let wsize: i32 = right -left +1;
-                if min_window_size == 0{
-                    min_window_size = wsize;
-                }
-                else {
+
                     min_window_size = min(min_window_size, wsize);
-                }
                 // min_window_size = right - left +1;
                 sum -=nums[left as usize];
                 left+=1;
             }            
         }
+        if min_window_size == i32::MAX{
+            return 0
+        }
+        else{
         return min_window_size;
+
+        }
     }
 }
