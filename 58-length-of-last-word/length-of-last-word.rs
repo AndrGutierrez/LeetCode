d11aaf2c19e15c6e46325f0e35aaa1353a9c8a7e
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-01-26 10:39:45 -0400

    Time: 0 ms (100.00%) | Memory: 2.3 MB (14.57%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.rs
@@ -0,0 +1,32 @@
+/*
+- substring: sliding window?
+- returns i32, length of last word
+- is current a space?
+    yes:
+        wsize = 0
+        go next
+    no: wsize+=1
+*/
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let mut window_sizes: Vec<i32> = Vec::<i32>::new();
+        let mut chars: Vec<char> = s.chars().collect();
+        let mut current_size = 0;
+        for c in chars {
+            if c == ' '{
+                current_size = 0;
+            }
+            else {
+                current_size+= 1;
+            }
+                            if current_size > 0{
+                    window_sizes.push(current_size);
+                }
+
+            //println!("{:?}, {:?}", c,  current_size);
+        }
+        let mut last = window_sizes.len() as i32 - 1;
+        //println!("{:?}", window_sizes[last as usize]);
+        return window_sizes[last as usize];
+    }
+}
