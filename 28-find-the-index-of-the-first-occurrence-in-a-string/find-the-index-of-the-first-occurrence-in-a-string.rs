436e9796b4d292e7fed293eb3453bb58b536e9bf
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-02-02 22:33:50 -0400

    Time: 25 ms (6.22%) | Memory: 2.4 MB (4.38%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.rs
@@ -0,0 +1,62 @@
+/* aguja en un pajar
+el indice de la primera aguja en el pajar, o -1 si no se encuentra
+
+la ajuga es un string, 
+
+sliding window
+
+if the current character if the same as haystacks ith character, i+=1
+if i >= len(needle): return left as the index of the window
+
+ continue
+else: left = right, go next
+
+return -1
+
+*/
+
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        let mut left = 0;
+        let mut right = 0;
+        let n = haystack.len() as i32;
+        let hchars: Vec<char>= haystack.chars().collect();
+        let nchars: Vec<char> = needle.chars().collect();
+        let mut i = 0;
+        while right < n{
+            let current = hchars[right as usize];
+            let needlechar =nchars[i as usize];
+            println!("{:?}, {:?}", left, right);
+
+            if  current ==  needlechar{
+                //println!("{:?}, {:?}", current, needlechar);
+                //println!("{:?}", left);
+                //println!("{:?}, {:?}", left, right);
+
+                i+=1;
+                if i >= nchars.len(){
+                    return left;
+                }
+            right+=1;
+
+            }
+            else {
+                // println!("{:?}, {:?}, {:?}", hchars[left as usize], nchars[0 as usize], left);
+                
+                left+=1;
+                if left >=n {return -1}
+                while (hchars[left as usize] !=  nchars[0 as usize]) && left <=n {
+
+                    left+=1;
+                    if left >=n {return -1}
+                }
+                right = left;
+
+                i=0;
+
+            }
+
+        }
+        return -1;
+    }
+}
