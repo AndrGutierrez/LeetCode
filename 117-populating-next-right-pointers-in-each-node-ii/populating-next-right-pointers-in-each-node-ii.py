3f45acf277d73bfbbebe887c9630247cf3faaf87
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-02-13 23:11:51 -0400

    Time: 46 ms (74.47%) | Memory: 20.5 MB (20.42%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,41 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+populate, the input is aa node
+
+looks like BFS
+
+# is the end of the level
+
+visit node
+
+the queue for this one is something like¨
+[1,2,3,4,5,7]
+
+so the right element is the right element in the queue
+"""
+from collections import deque
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        q = deque()
+        q.append(root)
+        while q:
+            current_level = []
+            level_size = len(q)
+            for element in range(level_size):
+                node = q.popleft()
+                if node:
+                    current_level.append(node)
+                    q.append(node.left)
+                    q.append(node.right)
+                    
+                    
+            for i, node in enumerate(current_level):
+                if i+1 < len(current_level):
+                    node.next = current_level[i+1]
+        return root
