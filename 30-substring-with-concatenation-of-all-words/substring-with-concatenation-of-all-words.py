f56b8542bf9e36ccc4568eb6f8fa45fc66feb3fa
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-03-15 16:08:25 -0400

    Time: 35 ms (41.54%) | Memory: 20.1 MB (35.15%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,31 +1,52 @@
-class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+from collections import Counter
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        word_count = Counter(words)
         n = len(s)
         k = len(words)
         word_length = len(words[0])
-        substring_size = word_length * k
-        word_count = collections.Counter(words)
-
-        def check(i):
-            # Copy the original dictionary to use for this index
-            remaining = word_count.copy()
-            words_used = 0
-
-            # Each iteration will check for a match in words
-            for j in range(i, i + substring_size, word_length):
-                sub = s[j : j + word_length]
-                if remaining[sub] > 0:
-                    remaining[sub] -= 1
-                    words_used += 1
-                else:
-                    break
-
-            # Valid if we used all the words
-            return words_used == k
-
+        substring_size = word_length*k
         answer = []
-        for i in range(n - substring_size + 1):
-            if check(i):
-                answer.append(i)
+
+        def sliding_window(left):
+            words_found = collections.defaultdict(int)
+            words_used = 0
+            excess_word = False
+
+            for right in range(left, n, word_length):
+                if right+word_length > n:
+                    break
+                sub = s[right:right+word_length]
+                if sub not in word_count:
+                    words_found = collections.defaultdict(int)
+                    words_used = 0
+                    excess_word = False
+                    left = right + word_length
+                else:
+                    while right - left == substring_size or excess_word:
+                        leftmost = s[left:left+word_length]
+                        left+=word_length
+                        words_found[leftmost] -=1
+                        if words_found[leftmost] == word_count[leftmost]:
+                            excess_word = False
+                        else:
+                            words_used -=1
+    
+                    words_found[sub]+=1
+                    if words_found[sub] <=word_count[sub]:
+                        words_used+=1
+                    else:
+                        excess_word=True
+                        
+                    if words_used == k and not excess_word:
+                        answer.append(left)
+    
+        for i in range(word_length):
+            sliding_window(i)
 
         return answer
