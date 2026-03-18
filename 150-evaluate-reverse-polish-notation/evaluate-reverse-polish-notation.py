60c523e4cc8948f033bd37b19933a068109942ef
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-03-18 15:45:21 -0400

    Time: 3 ms (57.16%) | Memory: 20.5 MB (88.77%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -0,0 +1,32 @@
+from collections import deque
+import math
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        if len(tokens) == 1:
+            return int(tokens[0])
+        q = deque()
+        ans = math.inf
+        for token in tokens:
+            match token:
+                case '*':
+                    current = int(q.pop())
+                    last = int(q.pop())
+                    q.append(current*last)
+                case '+':
+                    current = int(q.pop())
+                    last = int(q.pop())
+                    q.append(current+last)
+
+                case '-':
+                    current = int(q.pop())
+                    last = int(q.pop())
+                    q.append(last-current)
+
+                case '/':
+                    current = int(q.pop())
+                    last = int(q.pop())
+                    q.append(int(last/current))
+
+                case _:
+                    q.append(token)
+        return q.pop()
