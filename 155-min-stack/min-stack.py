f51d62f27b0f4b94255d42d51e7f37de48b932b9
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-03-17 23:26:50 -0400

    Time: 15 ms (12.00%) | Memory: 23.1 MB (6.09%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, val, node = None, prev = None):
+        self.val = val
+        self.next = node
+        self.prev = prev
+        self.smallest = None
+        self.largest = None
+
+class MinStack:
+
+    def __init__(self):
+        # self.largest = None
+        self.smallest = None
+        self.current = None
+        
+
+    def push(self, val: int) -> None:
+        if self.current:
+            new_node = Node(val, None, self.current)
+            # new_node.largest = max(self.current.largest, val)
+
+            new_node.smallest = min(self.current.smallest, val)
+
+            self.current.next = new_node
+            self.current = self.current.next
+        else:
+            self.current = Node(val)
+            # self.current.largest = val
+            self.current.smallest = val
+            
+        
+    def pop(self) -> None:
+        self.current = self.current.prev
+        if self.current: self.current.next = None
+        
+
+    def top(self) -> int:
+        return self.current.val
+        
+
+    def getMin(self) -> int:
+        return self.current.smallest
+        
+"""
+val, min, max
+-2, -2, -2
+0, -2, 0
+-1, -2, 0
+"""
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
