1e179ed7b7ac989c84efada17808c9b467c3575b
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-05-04 22:47:18 -0400

    Time: 2 ms (77.47%) | Memory: 21 MB (18.63%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        for num in nums:
+            if counts[num] == 1: return num
