10502575e1778d861049daaadcb18c29170fd04f
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-05-04 22:34:22 -0400

    Time: 45 ms (64.97%) | Memory: 19.2 MB (28.81%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -2,9 +2,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         binary_string= str(bin(n))[2:]
         remaining = 32 - len(binary_string)
-        while remaining != 0:
-            remaining-=1
-            binary_string = f"0{binary_string}"
+        binary_string = ("0" * remaining) + binary_string
         reversed_binary = "".join(reversed(binary_string))
         res= int(reversed_binary, 2)
         return res
