bfba4f72c39f07074876bd46dcbf0fd675ddde3e
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-05-07 01:03:57 -0400

    Time: 14 ms (83.26%) | Memory: 20.7 MB (7.20%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,41 +1,6 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-"""
-merge into sorted
-
-ok so there can be lots of linked list so this probably has to be O(n) or less
-
-lists are already sorted
-
-mmm heaps might be handy
-
-we could insert into a heap, and then convert? no that's heapsort and thats nlogn
-
-lists might be different sizes
-
-can we do some kind of three pointer approach? maybe too much, is there a way to reduce to logn?
-
-no, we cannot use pointers(?) this is a linked list... though converting list to array might not be that hard
-
-I mean i think the minimum time we can get for this is O(nk)
-
-if we change the dimension?
-
-like
-1 4 4
-
-1 3 5
-
-2 6
-"""
-
 import heapq
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+class Solution(object):
+    def mergeKLists(self, lists):
         heap = []
         heapq.heapify(heap)
 
@@ -43,14 +8,17 @@ class Solution:
         for lst in lists:
             if not lst: continue
             current = lst
-            heapq.heappush(heap, current.val)
+            l.append(current.val)
             while current.next:
                 current = current.next
-                heapq.heappush(heap, current.val)
+                l.append(current.val)
+
         head = ListNode()     
         current = head
-        for i in range(len(heap)):
-            current.next = ListNode(heapq.heappop(heap))
+        l = sorted(l)
+
+        for item in l:
+            current.next = ListNode(item)
             current = current.next
 
         return head.next
