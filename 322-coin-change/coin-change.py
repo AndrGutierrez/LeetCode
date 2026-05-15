812ee5ce619d9e7f22a8098b5a90beb151d51565
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-05-14 23:42:11 -0400

    Time: 496 ms (72.95%) | Memory: 19.6 MB (52.44%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -62,7 +62,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [math.inf] * (amount + 1)
         dp[0] = 0
-        coins = sorted(coins)
+        # coins = sorted(coins)
         for i in range(1, amount+1):
             remaining = i
             for coin in coins:
