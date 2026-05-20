12e9d9e04beb2189b5c620421487fe6acf0ad7af
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-05-20 16:16:43 -0400

    Time: 11 ms (56.64%) | Memory: 21.3 MB (80.16%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -21,19 +21,17 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        dp = [[float("inf") for _ in range(n)] for i in range(m)]
+        dp = [[float("inf")] *n for i in range(m)]
         dp[0][0] = grid[0][0] 
         for i in range(m):
             for j in range(n):
                 if i == 0 and j > 0:
                     dp[i][j] = dp[i][j-1] + grid[i][j]
-                    print()
                 if j == 0 and i > 0:
                     dp[i][j] = dp[i-1][j] + grid[i][j]
                 elif i>0 and j>0:
                     dp[i][j] = min(dp[i-1][j] +  grid[i][j], dp[i][j-1] + grid[i][j])
 
                     
-        print(dp)
 
         return dp[-1][-1]
