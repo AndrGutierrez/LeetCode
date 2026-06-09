a98f5d5a4ea3452fc0129c04042f2bb1ac78c071
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-06-08 22:19:02 -0400

    Time: 8 ms (24.12%) | Memory: 20.4 MB (61.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -12,17 +12,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        right = 1
-        i = 0 
+        left = 0 #writer pointer
+        right = 0 #reader pointer
         n = len(nums)
         while right < n:
-            if nums[left] != 0:
+            if nums[right] != 0:
+                nums[left] = nums[right]
                 left+=1
-                right = left +1
-            elif nums[left] == 0 and nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left = left+1
-                right = left + 1
-            else:
-                right +=1
+            right +=1
+            
+        for i in range(left, right):
+            nums[i] = 0
