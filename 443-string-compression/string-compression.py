31e6d6b131d8fca9ee854c18597e970b6c4be14f
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-06-08 21:46:25 -0400

    Time: 3 ms (45.90%) | Memory: 19.3 MB (55.89%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,60 @@
+"""
+return only the length of the compressed array
+
+groups that are longer than 10 will be split into 2 multiple characters
+
+ok so we just get the latest, ake a count, convert the count to string, append each character, and return len, ez
+
+ok so how do we do it in place, I can think of how replacing but removing... well the input is fairly small so we can just remove everythjing after a given pointer
+"""
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1: return 1
+        latest = chars[0]
+        count = 1
+        n = len(chars)
+
+        pointer = 0
+        for i in range(1, n):
+            c = chars[i]
+            # if i == n-1 and c != latest: 
+# 
+            #     count+=1
+            #     pointer +=1
+            #     chars[pointer] = c
+            #     pointer =1
+
+
+            if c != latest: # or (i == n-1 and c == latest):
+                # if i == n-1:
+                #     count+=1
+                chars[pointer] = latest
+                pointer +=1
+                if count > 1:
+                    strcount = str(count)
+                    for countchar in strcount:
+                        chars[pointer] = countchar 
+                        pointer +=1
+                latest = c
+                count = 1
+
+            else:
+                count+=1
+            
+        if chars[-1] == latest:
+            chars[pointer] = latest
+            pointer +=1
+            if count > 1:
+                strcount = str(count)
+                for countchar in strcount:
+                    chars[pointer] = countchar 
+                    pointer +=1
+            latest = c
+            count = 1
+
+        remove = len(chars) - pointer 
+        print(chars)
+        for i in range(remove):
+            chars.pop()
+        return len(chars)
+        
