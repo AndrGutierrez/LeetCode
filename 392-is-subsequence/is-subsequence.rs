8d50ffb64c570054801def0f3afb4a9fdae8d582
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-06-10 23:21:28 -0400

    Time: 0 ms (100.00%) | Memory: 2.3 MB (18.18%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.rs
@@ -0,0 +1,28 @@
+/*
+ok i think we can do this in O(n) time by keeping a pointerin thelatest not found letter and a pointerin t, if we traverse s we're done
+*/
+impl Solution {
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        let mut left: usize = 0;
+        let right: i32 = 0;
+        let k = s.len();
+        if k == 0 {
+            return true
+        }
+        let target = k ;
+        for c in t.chars() {
+            let current_s: char = s.chars().nth(left).unwrap();
+
+            if current_s == c {
+            println!("{:?}, {:?}", current_s, c);
+
+                left+=1;
+                if left == target {
+                    return true
+                }
+
+            }
+        }
+        return false;
+    }
+}
