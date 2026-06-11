1f57a1192bc4768093618875139cf4109fb486a7
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-06-10 23:23:33 -0400

    Time: 0 ms (100.00%) | Memory: 2.2 MB (67.53%) - LeetSync

--- 392-is-subsequence/is-subsequence.rs
+++ 392-is-subsequence/is-subsequence.rs
@@ -4,24 +4,19 @@ ok i think we can do this in O(n) time by keeping a pointerin thelatest not foun
 impl Solution {
     pub fn is_subsequence(s: String, t: String) -> bool {
         let mut left: usize = 0;
-        let right: i32 = 0;
         let target = s.len();
-        if target == 0 {
-            return true
-        }
+        if target == 0 { return true }
+
         for c in t.chars() {
-
             let current_s: char = s.chars().nth(left).unwrap();
-
             if current_s == c {
-
                 left+=1;
                 if left == target {
                     return true
                 }
-
             }
         }
+
         return false;
     }
 }
