6eaca5e76a69bacd17ec181e599090537e25ef87
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-06-24 23:04:54 -0400

    Time: 83 ms (71.94%) | Memory: 62.4 MB (48.77%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -9,26 +9,14 @@ delete middle node and return head, it has to be in place
 """
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        n = 1
-        current = head 
+        if not head: return None
 
-        while current.next:
-            n+=1
-            current = current.next
-
-        curr = head
-        half = n//2
-
-        if n ==1 or n ==0: 
-            return None
+        slow = head
+        if not slow.next: return None
+        fast = head.next.next
         while True:
-            if not curr: break
-            if not curr.next: break
-            half-=1
-            if half == 0:
-                curr.next = curr.next.next
-
-            curr = curr.next
-
+            if not fast or not fast.next: break
+            slow = slow.next
+            fast = fast.next.next
+        slow.next = slow.next.next
         return head
-            
