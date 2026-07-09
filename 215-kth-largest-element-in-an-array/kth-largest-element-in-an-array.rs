02347d275b99d8c6299c8fabb41c19a65383648e
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-07-09 16:59:31 -0400

    Time: 20 ms (8.65%) | Memory: 3.5 MB (62.50%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.rs
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.rs
@@ -11,14 +11,9 @@ impl Solution {
     pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
         let mut heap: BinaryHeap<i32> = BinaryHeap::from(nums);
         for i in 0..k {
-            let val = heap.pop();
-            match val {
-                Some(v)=>{
-                    if i as i32 == k - 1 {
-                        return v;
-                    }
-                }
-                _ =>{}
+            let val = heap.pop().unwrap();
+            if i as i32 == k - 1 {
+                return val;
             }
         }
         return 0;
