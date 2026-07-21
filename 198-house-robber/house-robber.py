cf57182b15239b7b468618a209afbeb8d5aff684
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-07-20 23:33:35 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.02%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,34 +1,47 @@
 """
-maximum: some kind of optimization, greedy or dp
+ok, an optimization problem
 
-ok so the first approach would be some kind of backtracking and recursion, we take a
-decision each time of rob vs not to rob, we can optimize later
+we cannot rob two adjacent houses
+
+this could be dp, can we divide it into smaller subproblems?
+
+for example
+
+[1, 2, 1000, 1002]
+
+[1, 2, 1001, 1004]
+
+I mean can we just make this the sum of the evens and sum of the odds? 
+
+is there a case where we cant do that??
+
+rigt cases like
+
+[2, 1, 1, 2]
+
+or even more extreme stuff i guess
+
+[1, 300, 1, 3, 100]
+
+where it's better to pick just the two 
+
+so what we do is... well the input size of nums suggest maybe a O(n²), brute force is something crazy exponential
+but I think O(n²) is manageable, and if it isnt it might lead to a more efficient solution
+
+[1, 300, 2, 303, 400]
+
+so filling the last one in this case... i mean we just gotta get the biggest so far 
+that isn't adjacent to the current and sum what we have, or is there something better?
+
+and if it isn't better? it has to be by definition so...
 """
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # Recursive solution
-        # res = 0
-        # maximums = []
-        # def backtrack(curr, optimal, robbed):
-        #     if curr >= len(nums)-1:
-        #         nonlocal maximums
-        #         maximums.append(optimal)
-        #         
-        #     for i in range(curr, len(nums)):
-        #         if i-1 not in robbed and i+1 not in robbed:
-        #             robbed.add(i)
-        #             backtrack(i+1, optimal+nums[i], robbed)
-        #             robbed.remove(i)
-        # backtrack(0, 0, set())
-        # res = max(maximums)
-        # return res
-        # Tabulation
-        if not nums: return 0
+        res = []
         if len(nums) == 1: return nums[0]
-        n = len(nums)
-        robbed = [0] * n
-        robbed[0], robbed[1] = nums[0], max(nums[0], nums[1])
-
-        for i in range(2, n):
-            robbed[i] = max(robbed[i-1], robbed[i-2]+nums[i])
-        return robbed[-1]
+        for i in range(len(nums)):
+            if i == 0 or i ==1:
+                res.append(nums[i])
+            else: 
+                res.append(max(max(res[:i - 1]) + nums[i], res[-1]))
+        return max(res[-1], res[-2])
