9eb467a9b02d0478b1d6cf6d4cc9400b0917c97a
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-07-26 19:14:00 -0400

    Time: 378 ms (43.71%) | Memory: 44.9 MB (34.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -20,11 +20,19 @@ class Solution:
         n = len(text1)
         m = len(text2)
         dp = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if text2[i] == text1[j]:
-                    dp[i][j] = dp[i+1][j+1] + 1
+        # for i in range(m - 1, -1, -1):
+        #     for j in range(n - 1, -1, -1):
+        #         if text2[i] == text1[j]:
+        #             dp[i][j] = dp[i+1][j+1] + 1
+        #         else:
+        #             dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                # print(i, j)
+                if text2[i - 1] == text1[j - 1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-
-        return dp[0][0]
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        for row in dp:
+            print(row)
+        return dp[m][n]
