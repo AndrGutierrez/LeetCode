06312b2ef5ecaf8cda7328bc5dd789d0a0bb8dbf
Andrés Gutierrez <12andresgutierrez04@gmail.com>
2026-08-09 20:38:13 -0400

    Time: 168 ms (14.04%), Space: 53.6 MB (32.56%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,35 @@
+"""
+minimum amout of balloons needed
+
+so this is an interval problem we maybe want to sort the intervals first, then we would have like
+
+[1, 6], [2, 8], [7, 12], [10,16]
+
+if the intervals overlap we need an arrow, then we would need the amount of balloons - overlapping intervals?
+
+ok so the problem is if several overlap how did we that? i think it was kind of a k 
+
+we got coordinates x1, x2, if x1 > k it doesnt overlap and else we set k to x2
+
+well its not - the amount of overlapping intervals but the amount of ...
+
+this might be greedy?
+
+ok so we set like an end, to the first, if the next start before the current end they overlap and we -=1, if not we set the new k
+"""
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key= lambda x: x[0])
+        k = -float('inf')
+        overlapping = 0
+
+        print(points)
+        for x1, x2 in points:
+            if x1 <= k:
+                overlapping += 1
+                k = min(k, x2)
+            else:
+                k = x2
+
+        return len(points) - overlapping
